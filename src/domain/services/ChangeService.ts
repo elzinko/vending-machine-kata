e059f090362b84a4ce5efb8f40a40888512cee99
Thomas Couderc <thomas.couderc@gmail.com>
2024-05-20 16:36:06 +0200

    feat(make-change): add check if amount is multiple of smallest coin for change

--- src/domain/services/ChangeService.ts
+++ src/domain/services/ChangeService.ts
@@ -10,13 +10,14 @@ export class ChangeService {
    * ChangeService.makeChange(41); // ['QUARTER', 'DIME', 'NICKEL', 'NICKEL']
    * ChangeService.makeChange(0); // []
    * ChangeService.makeChange(1); // ['PENNY']
+   * @throws Error if the amount is not a multiple of the smallest coin value
    * @remarks
    * This method uses a greedy algorithm to make change. It sorts the coins by value in descending order and then iterates over the coins, adding the maximum number of each coin to the result until the amount is zero.
    */
-  static makeChange(amount: number): CoinType[] {
-    const coinTypes = Object.keys(CoinValues) as CoinType[];
+  static makeChange(amount: number, allowedCoins: CoinType[]): CoinType[] {
+    ChangeService.assertAmountIsMultipleOfSmallestCoin(amount, allowedCoins);
 
-    return coinTypes
+    return allowedCoins
       .sort(
         (a, b) =>
           CoinValues[b as keyof typeof CoinValues] -
@@ -29,5 +30,28 @@ export class ChangeService {
         amount -= count * CoinValues[coin as keyof typeof CoinValues];
         return acc.concat(Array(count).fill(coin));
       }, [] as CoinType[]);
+  }
+
+  /**
+   * Check if an amount is a multiple of the smallest coin value, given a list of allowed coins.
+   * @param amount the amount to check
+   * @param allowedCoins list of allowed coins
+   */
+  static assertAmountIsMultipleOfSmallestCoin(
+    amount: number,
+    allowedCoins: CoinType[]
+  ) {
+    const smallestCoin = allowedCoins.reduce(
+      (smallest, coin) =>
+        CoinValues[coin as keyof typeof CoinValues] <
+        CoinValues[smallest as keyof typeof CoinValues]
+          ? coin
+          : smallest,
+      allowedCoins[0]
+    );
+
+    if (amount % CoinValues[smallestCoin as keyof typeof CoinValues] !== 0) {
+      throw new Error('Amount must be a multiple of the smallest coin');
+    }
   }
 }
--- tests/domain/services/ChangeService.test.ts
+++ tests/domain/services/ChangeService.test.ts
@@ -1,35 +1,42 @@
 import {ChangeService} from '../../../src/domain/services/ChangeService';
-import {Coin, CoinType, CoinValues} from '../../../src/domain/models/Coin';
+import {CoinType, CoinValues} from '../../../src/domain/models/Coin';
 
 describe('ChangeService', () => {
+  const allowedCoinsExample = ['QUARTER', 'DIME', 'NICKEL'] as CoinType[];
+
   test('should return an empty array when amount is 0', () => {
-    const result = ChangeService.makeChange(0);
+    const result = ChangeService.makeChange(0, allowedCoinsExample);
     expect(result).toEqual([]);
   });
 
   test('should return the correct change for amount 75', () => {
-    const result = ChangeService.makeChange(75);
+    const result = ChangeService.makeChange(75, allowedCoinsExample);
     const expected: CoinType[] = ['QUARTER', 'QUARTER', 'QUARTER'];
     expect(result).toEqual(expected);
   });
 
   test('should return the correct change using biggest coins', () => {
     const amount = 5 * CoinValues.DIME + CoinValues.NICKEL;
-    const result = ChangeService.makeChange(amount);
+    const result = ChangeService.makeChange(amount, allowedCoinsExample);
     const expected: CoinType[] = ['QUARTER', 'QUARTER', 'NICKEL'];
     expect(result).toEqual(expected);
   });
 
-  // Does not work as 41 and 1 are allowed amounts that can be computed when using the CoinValues (penny not in it)
-  /* test('should return the correct change for amount 45', () => {
-    const result = ChangeService.makeChange(41);
-    const expected: CoinType[] = ['QUARTER', 'DIME', 'NICKEL', 'NICKEL'];
-    expect(result).toEqual(expected);
+  test('should return the correct change for amount 1', () => {
+    expect(() => ChangeService.makeChange(1, allowedCoinsExample)).toThrow(
+      'Amount must be a multiple of the smallest coin'
+    );
   });
 
-  test('should return the correct change for amount 1', () => {
-    const result = ChangeService.makeChange(1);
-    const expected: CoinType[] = ['PENNY'];
+  test('should thorw an error when asking to give monney back', () => {
+    expect(() => ChangeService.makeChange(51, allowedCoinsExample)).toThrow(
+      'Amount must be a multiple of the smallest coin'
+    );
+  });
+
+  test('should return the correct change for amount 45', () => {
+    const result = ChangeService.makeChange(45, allowedCoinsExample);
+    const expected: CoinType[] = ['QUARTER', 'DIME', 'DIME'];
     expect(result).toEqual(expected);
-  }); */
+  });
 });
